5e80cad4a5dc675ac1c8755d53e757341c858737
Ajay_Pantham <ajaykumar.pantham@quantiphi.com>
2020-07-02 00:34:25 +0530

    commiting code for EST business hours logic

--- lifecycle-configs/autostop.py
+++ lifecycle-configs/autostop.py
@@ -4,6 +4,7 @@ import getopt, sys
 import urllib3
 import boto3
 import json
+import pytz
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -23,15 +24,40 @@ helpInfo = """-t, --time
 -h, --help
     Help information
 """
+flag=0
+
+now = datetime.now()
+print("CURRENT Time is : ",now)
 
 
-x = datetime.now()
-print("CURRENT Time is : ",x)
-H_now=x.hour
-M_now=x.minute
-S_now=x.second
+# This contains the local timezone i.e , UTC
+local= pytz.timezone('UTC')
 
-OUT_HOUR=9
+#replace function is used to convert time naive datatime object 'now' into a timezone aware object.
+now = now.replace(tzinfo = local)
+# prints a timezone aware datetime
+print("DEFAULT time with timezone is :",now)
+
+
+# astimezone is used to convert datetime from one timezone to another
+#tz = pytz.timezone('America/Toronto')
+tz = pytz.timezone('Asia/Kolkata')
+now = now.astimezone(tz)
+#print ("EST time is:",now)
+print ("IST time is:",now)
+
+H_now=now.hour
+M_now=now.minute
+S_now=now.second
+
+print("H_now is :",H_now)
+
+IN_HOUR=9
+IN_MIN=00
+IN_SEC=00
+
+
+OUT_HOUR=17
 OUT_MIN=00
 OUT_SEC=00
 
@@ -83,12 +109,26 @@ def get_notebook_name():
     return _logs['ResourceName']
 
 
-H_sec=(H_now - OUT_HOUR)*3600
-M_sec=(M_now - OUT_MIN)*60
-Sec=(S_now - OUT_SEC)
-Total_sec = H_sec + M_sec + Sec
-print("Total sec is :",Total_sec)
-if  Total_sec > time : 
+
+if H_now>OUT_HOUR and H_now<24:
+    H_sec=(H_now - OUT_HOUR)*3600
+    M_sec=(M_now - OUT_MIN)*60
+    Sec=(S_now - OUT_SEC)
+    Total_night_sec = H_sec + M_sec + Sec
+    print("Total night sec is :",Total_night_sec)
+    if Total_night_sec >= time:
+        flag=1 
+elif H_now>=0 & H_now<=IN_HOUR:
+    H_sec=(IN_HOUR - H_now)*3600
+    M_sec=(IN_MIN - M_now)*60
+    Sec=(IN_SEC - S_now)
+    Total_mrng_sec = H_sec + M_sec + Sec
+    print("time (in sec) to enter into start time of business hours is : ",Total_mrng_sec)
+    if Total_mrng_sec>=-60:
+        flag=1
+
+#we are interested in stopping the instance if it is idle for 'x' time outside the business hours.
+if  flag==1: 
     # This is hitting Jupyter's sessions API: https://github.com/jupyter/jupyter/wiki/Jupyter-Notebook-Server-API#Sessions-API
     response = requests.get('https://localhost:'+port+'/api/sessions', verify=False)
     data = response.json()
